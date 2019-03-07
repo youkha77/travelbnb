0bcacba99cb0014afd052dffab868fc34da03391
khai <khaiway_la@hotmail.fr>
2019-03-07 18:58:20 +0100

    securité et réservation avec booking

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,6 +7,8 @@ use Faker\Factory;
 use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
+
+use App\Entity\Booking;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -97,10 +99,35 @@ class AppFixtures extends Fixture
                 $manager->persist($image);
             }
 
-            $manager->persist($ad);
+            // Gestion des réservations
+        for($j = 1; $j <= mt_rand(0, 10); $j++){
+            $booking = new Booking();
 
+            $createAt = $faker->dateTimeBetween('-6 months');
+            $startDate = $faker->dateTimeBetween('-3 months');
+        // Gestion de la date de fin
+            $duration = mt_rand(3, 10);
+            $endDate  = (clone $startDate)->modify("+$duration days");
+            $amount = $ad->getPrice() * $duration;
+
+            $booker = $users[mt_rand(0, count($users) -1)];
+            $comment = $faker->paragraph();
+
+            $booking->setBooker($booker)
+                    ->setAd($ad)
+                    ->setStartDate($startDate)
+                    ->setEndDate($endDate)
+                    ->setCreateAt($createAt)
+                    ->setAmount($amount)
+                    ->setComment($comment);
+                
+            $manager->persist($booking);
+
+            
         }
-
+            $manager->persist($ad);
+        }
+        
         $manager->flush();
     }
 }
---
+++ src/Entity/Booking.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BookingRepository")
+ * @ORM\HasLifecycleCallBacks()
+ */
+class Booking
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="bookings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $booker;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Ad", inversedBy="bookings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $ad;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Assert\Date(message="Attention, la date d'arrivée doit etre au bon format !")
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Assert\Date(message="Attention, la date départ doit etre au bon format !")
+     */
+    private $endDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $amount;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $comment;
+   
+    /**
+     * Callback appelé à chaque fois qu'on crée une réservation
+     * 
+     * @ORM\PrePersist
+     *
+     * @return void
+     */
+    public function prePersist(){
+        if(empty($this->createAt)) {
+            $this->createAt = new \DateTime();
+        }
+
+        if(empty($this->amount)) {
+            // prix de l'annonce * nombre de jour
+            $this->amount = $this->ad->getPrice() * $this->getDuration();
+        }
+    }
+
+    public function isBookableDates(){
+        // 1) Il faut connaitre les dates qui sont impossibles pour l'annonce
+        $notAvailableDays = $this->ad->getNotAvailableDays();
+        // 2) Il faut comparer les dates choisies avec les dates impossibles
+        $bookingDays = $this->getDays();
+
+        $formatDay = function($day){
+            return $day->format('Y-m-d');
+        };
+
+        // Tableau des chaines de caractères de mes journées
+        $days             = array_map($formatDay, $bookingDays);
+        $notAvailable = array_map($formatDay, $notAvailableDays);
+
+        foreach($days as $day){
+            if(array_search($day, $notAvailable) !== false) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Permet de récupérer un tableau des journées qui correspondent à ma reservation
+     *
+     * @return array Un tableau d'objets DateTime représentant les jours de la réservation
+     */
+    public function getDays(){
+        $resultat = range(
+            $this->startDate->getTimestamp(),
+            $this->endDate->getTimestamp(),
+            24 * 60 * 60 
+        );
+
+        $days = array_map(function($dayTimestamp){
+            return new \DateTime(date('Y-m-d', $dayTimestamp));
+        }, $resultat);
+        return $days;
+    }
+
+    public function getDuration(){
+        $diff = $this->endDate->diff($this->startDate);
+        return $diff->days;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBooker(): ?User
+    {
+        return $this->booker;
+    }
+
+    public function setBooker(?User $booker): self
+    {
+        $this->booker = $booker;
+
+        return $this;
+    }
+
+    public function getAd(): ?Ad
+    {
+        return $this->ad;
+    }
+
+    public function setAd(?Ad $ad): self
+    {
+        $this->ad = $ad;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeInterface $createAt): self
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): self
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+}
