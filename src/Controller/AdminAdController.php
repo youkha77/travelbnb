22f0e20c198b6ca2610a0f0b11769787ba19c833
khai <khaiway_la@hotmail.fr>
2019-03-11 19:25:16 +0100

    gestion des annonces administrateur

---
+++ src/Controller/AdminAdController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Form\AnnonceType;
+use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class AdminAdController extends AbstractController
+{
+    /**
+     * @Route("/admin/ads", name="admin_ads_index")
+     */
+    public function index(AdRepository $repo)
+    {
+        return $this->render('admin/ad/index.html.twig', [
+            'ads' => $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'edition
+     * 
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     *
+     * @param Ad $ad
+     * @return void
+     */
+    public function edit(Ad $ad, Request $request, ObjectManager $manager){
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig',[
+            'ad' => $ad,
+            'form' => $form->createView()
+        ]);
+    }
+}
